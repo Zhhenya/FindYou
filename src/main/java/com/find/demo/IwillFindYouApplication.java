fa79890e82c3f3bb03393b5e473d9aea6c2aa680
Zenya <ushakovazenya@gmail.com>
2018-03-30 23:15:43 +0300

    Initial commit

---
+++ src/main/java/com/find/demo/IwillFindYouApplication.java
@@ -0,0 +1,21 @@
+package com.find.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class IwillFindYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwillFindYouApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+    public String say(){
+	    return "Иди спать!!! не еби мозг!Спокойной ночи";
+    }
+
+}
